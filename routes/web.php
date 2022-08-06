7430a0798c7ac50dabf2cc81df4f12fc981784c1
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-08-06 08:56:20 -0300

    Enviando parametros na rota

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,9 @@ Route::get('/', [PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [ContatoController::class, 'contato']);
+
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}',
+    function(string $nome, string $categoria, string $assunto, string $mensagem) {
+        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+    }
+);
