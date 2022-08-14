255694295cee05eabf61ed041a7f4ba9d8d5db4e
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-08-14 05:19:57 -0300

    Nomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -16,29 +16,29 @@ use App\Http\Controllers\SobreNosController;
 |
 */
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 
 Route::get('/login', function(){
     return 'Login';
-});
+})->name('site.login');
 
 //Agrupando Rotas
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){
         return 'Clientes';
-    });
+    })->name('app.clientes');
 
     Route::get('/fornecedores', function(){
         return 'Fornecedores';
-    });
+    })->name('app.fornecedores');
 
     Route::get('/produtos', function(){
         return 'Produtos';
-    });
+    })->name('app.produtos');
 
 });
 
