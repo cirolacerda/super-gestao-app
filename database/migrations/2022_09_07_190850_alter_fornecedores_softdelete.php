1600c8972231717535c387467f08a4122c2e32eb
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-09-07 16:28:48 -0300

    Criando migration fornecedores softdelete

---
+++ database/migrations/2022_09_07_190850_alter_fornecedores_softdelete.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //criando softdelete
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         //deletando softdelete
+         Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+};
