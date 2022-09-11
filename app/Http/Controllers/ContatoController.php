379454f6720a82095690c745671452a7260c63c6
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-09-11 18:15:53 -0300

    Gravando dados do formulario contato no BD

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,13 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
 
-        var_dump($_POST);
+        //print_r($request->all());
+        //var_dump($_POST);
+
+        /*
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save();
+        */
+         /*
+        $contato = new SiteContato();
+        $contato->fill($request->all());
+        $contato->save();
+        */
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+
+        print_r($contato->getAttributes());
+
         return view('site.contato');
+
+
     }
+
 }
