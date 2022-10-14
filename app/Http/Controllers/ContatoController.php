3fbef6ff1162eeaac4c9c6519bd3dd5acceed06b
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-10-14 14:59:22 -0300

    Persistindo dados site contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -48,10 +48,13 @@ class ContatoController extends Controller
             'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
             'email' => 'email',
-            'motivo_contato' => 'required',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required'
 
         ]);
+
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
 
     }
 
