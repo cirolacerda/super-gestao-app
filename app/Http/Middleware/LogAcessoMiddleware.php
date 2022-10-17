cf05a098c6ea24c627b96241ea56ef9fbe12b2b4
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-10-17 06:07:45 -0300

    Criando middleware log de acesso

---
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\LogAcesso;
+use Closure;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\Request;
+
+class LogAcessoMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        //return $next($request);
+
+        $ip = $request->server->get('REMOTE_ADDR');
+        $rota = $request->getRequestUri();
+
+        LogAcesso::create(['log' => "IP $ip requisitou a rota $rota"]);
+
+        return Response('Chegamos no middleware e finalizamos no próprio');
+    }
+}
