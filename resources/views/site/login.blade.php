9ce8a1797105fb0bd603d660fd45bc051b899b9d
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-10-24 13:16:56 -0300

    Exibindo mensagens de erros

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -14,8 +14,10 @@
             <form action={{ route('site.login') }} method="POST">
                 @csrf
 
-                <input name="usuario" type="text" placeholder="Usuário" class="borda-preta">
-                <input name="senha" type="password" placeholder="Senha" class="borda-preta">
+                <input name="usuario" value="{{ old('usuario')}}" type="text" placeholder="Usuário" class="borda-preta">
+                {{ $errors->has('usuario') ? $errors->first('usuario') : '' }}
+                <input name="senha" value="{{ old('senha')}}" type="password" placeholder="Senha" class="borda-preta">
+                {{ $errors->has('senha') ? $errors->first('senha') : '' }}
 
                 <button type="submit" class="borda-preta">Acessar</button>
             </form>
