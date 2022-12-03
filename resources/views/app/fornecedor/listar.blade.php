9f2f0ee9b57a95e431424a60b104be2559e6481a
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-12-03 20:07:10 -0300

    add: listagem de produtos

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -41,20 +41,45 @@
                                 <td><a href="{{ route('app.fornecedor.editar', $fornecedor->id) }}">Editar</a></td>
                                 <td><a href="{{ route('app.fornecedor.excluir', $fornecedor->id) }}">Excluir</a></td>
                             </tr>
+                            <tr>
+                                <td colspan="6">
+                                    <p>Lista de Produtos</p>
+                                    <table border="1" style="margin: 20px">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Nome</th>
+                                            </tr>
+                                        </thead>
+
+                                        <tbody>
+                                            @foreach ($fornecedor->produtos as $key => $produto)
+                                                <tr>
+                                                    <td>{{ $produto->id }}</td>
+                                                    <td>{{ $produto->nome }}</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+
+                                    </table>
+
+                                </td>
+                            </tr>
                         @endforeach
 
                     </tbody>
                 </table>
 
-                {{$fornecedores->appends($request)->links('pagination::bootstrap-4')}}
+                {{ $fornecedores->appends($request)->links('pagination::bootstrap-4') }}
                 <!--
+                                                            <br>
+                                                            {{ $fornecedores->count() }} - Total de registros por página
+                                                            <br>
+                                                            {{ $fornecedores->total() }} - Total de registros da consulta
+                                                            -->
                 <br>
-                {{$fornecedores->count()}} - Total de registros por página
-                <br>
-                {{$fornecedores->total()}} - Total de registros da consulta
-                -->
-                <br>
-                Exibindo {{ $fornecedores->count()}} fornecedor(es) de {{ $fornecedores->total()}} (de {{ $fornecedores->firstItem()}} a {{ $fornecedores->lastItem()}})
+                Exibindo {{ $fornecedores->count() }} fornecedor(es) de {{ $fornecedores->total() }} (de
+                {{ $fornecedores->firstItem() }} a {{ $fornecedores->lastItem() }})
 
             </div>
         </div>
