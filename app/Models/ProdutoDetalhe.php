0db5b1ee70bddbf016bb47dfa81ebe2d5110367f
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-12-03 15:55:14 -0300

    add: relacionamento belongsTo produto

--- app/Models/ProdutoDetalhe.php
+++ app/Models/ProdutoDetalhe.php
@@ -12,4 +12,9 @@ class ProdutoDetalhe extends Model
     protected $table = 'produtos_detalhes';
 
     protected $fillable = ['produto_id', 'comprimento', 'largura', 'altura', 'unidade_id'];
+
+    public function produto()
+    {
+        return $this->belongsTo(Produto::class);
+    }
 }
