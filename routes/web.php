9296b0695c9878013f82a1580bb6d632e15ce91f
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2022-12-17 22:15:50 -0300

    add:  rotas create e store pedido produto

--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,9 @@ Route::middleware('autenticacao')->prefix('/app')->group(function () {
 
     Route::resource('pedido', PedidoController::class );
 
-    Route::resource('pedido-produto', PedidoProdutoController::class );
+    //Route::resource('pedido-produto', PedidoProdutoController::class );
+    Route::get('pedido-produto/create/{pedido}', [PedidoProdutoController::class, 'create'])->name('pedido-produto.create');
+    Route::post('pedido-produto/store/{pedido}', [PedidoProdutoController::class, 'store'])->name('pedido-produto.store');
 });
 
 Route::fallback(function () {
